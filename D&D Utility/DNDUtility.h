c5abea413d62cf05e5cc68155ce3af2c28ab092b
Caleb Chervenka <83119836+CjPhoenix@users.noreply.github.com>
2022-11-10 12:42:02 -0600

    Initial Commit

---
+++ D&D Utility/DNDUtility.h
@@ -0,0 +1,18 @@
+//
+//  DNDUtility.h
+//  D&D Utility
+//
+//  Created by Caleb Chervenka on 11/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DNDUtility.
+FOUNDATION_EXPORT double DNDUtilityVersionNumber;
+
+//! Project version string for DNDUtility.
+FOUNDATION_EXPORT const unsigned char DNDUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DNDUtility/PublicHeader.h>
+
+
